34b1f05881c08e53a7ef48bdda7d5f5a40421afc
mujiu555 <Sihang.Sha23@student.xjtlu.edu.cn>
2025-08-22 03:39:09 +0800

    feat(hecto): Ch1~2
    Feature:
    1. .gitignore: ignore target dir
    2. src/main.rs: Ch1~2
    
    Request: #none

--- src/hecto/src/main.rs
+++ src/hecto/src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{stdin, stdout};
+use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+
+fn die(e: impl std::error::Error) {
+    panic!("{}", e);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _stdout = stdout().into_raw_mode().unwrap();
+    for k in stdin().keys() {
+        match k {
+            Ok(k) => match k {
+                Key::Char(c) => println!("{:?} ({:?}) \r", c as u8, c),
+                Key::Ctrl('q') => break,
+                Key::Ctrl(c) => println!("{:?} (Ctrl: '{}')\r", c as u8, c),
+                _ => println!("{:?}\r", k),
+            },
+            Err(e) => die(e),
+        }
+    }
 }
