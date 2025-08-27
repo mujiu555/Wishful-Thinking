4b2a256db2d2a5dfc10b82b1f35cbea0655fae57
mujiu555 <1310769510@qq.com>
2025-08-28 04:46:09 +0800

    feat: basic operation
    Feature: src/hecto/ ...
    1. src/*.rs: implement basic ui that can interactive with input
    
    TODO: src/hecto/ ...
    1. keymap
    2. hecto...
    
    Request: #none

--- src/hecto/src/cursor.rs
+++ src/hecto/src/cursor.rs
@@ -1,18 +1,40 @@
 #[derive(Debug)]
 struct Pos {
-  x: usize,
-  y: usize,
+    x: usize,
+    y: usize,
 }
 
 #[derive(Debug)]
 pub struct Cursor {
-  pos: Pos,
+    pos: Pos,
 }
 
 impl Default for Cursor {
-  fn default() -> Self {
-    Self {
-      pos: Pos { x: 0, y: 0 },
+    fn default() -> Self {
+        Self {
+            pos: Pos { x: 0, y: 0 },
+        }
     }
-  }
+}
+
+impl Cursor {
+    pub fn x(&self) -> usize {
+        self.pos.x
+    }
+
+    pub fn y(&self) -> usize {
+        self.pos.y
+    }
+}
+
+impl Cursor {
+    pub fn goto(&mut self, x: usize, y: usize) {
+        self.pos.x = x;
+        self.pos.y = y;
+    }
+
+    pub fn mv(&mut self, x: isize, y: isize) {
+        self.pos.x = (self.pos.x as isize + x) as usize;
+        self.pos.y = (self.pos.y as isize + y) as usize;
+    }
 }
--- src/hecto/src/main.rs
+++ src/hecto/src/main.rs
@@ -1,12 +1,14 @@
 mod buffer;
 mod cursor;
 mod editor;
+mod exp;
 mod terminal;
 pub use buffer::Buffer;
 pub use cursor::Cursor;
 pub use editor::Editor;
+pub use exp::INIT_EXPT;
 pub use terminal::Terminal;
 
 fn main() {
-    todo!();
+    editor::Editor::new().unwrap().run();
 }
