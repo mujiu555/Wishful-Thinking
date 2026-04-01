b21751371050572ec97fb66f71a1e139b21ba617
Github@mujiu555 <1310769510@qq.com>
2026-04-02 03:35:00 +0800

    doc(CIS194): course work for CIS 194 II

--- src/CIS194/src/LogAnalysis.hs
+++ src/CIS194/src/LogAnalysis.hs
@@ -1,9 +1,11 @@
 -- | OPTIONS_GHC -Wall -xc
 
 module CIS194.LogAnalysis where
-import Log
+
 import Data.Char (isDigit, digitToInt)
 import Data.Maybe (fromJust)
+
+import Log
 
 -- Exercise I
 
@@ -58,8 +60,59 @@ parse :: String -> [LogMessage]
 parse str = parseHelper (splitBy str '\n')
 
 -- Exercise II
+
+height :: MessageTree -> Int
+height Leaf = 0
+height (Node lt _ rt) = 1 + max (height lt) (height rt)
+
+balance :: MessageTree -> Int
+balance Leaf = 0
+balance (Node lt _ rt) = height lt - height rt
+
+rotate :: MessageTree -> MessageTree
+rotate Leaf = Leaf
+rotate tree@(Node ltree _ rtree)
+  | balance tree > 1 && balance ltree > 0  = right tree
+  | balance tree < -1 && balance rtree < 0 = left tree
+  | balance tree > 1 && balance ltree < 0  = leftright tree
+  | balance tree < -1 && balance rtree > 0 = rightleft tree
+  | otherwise                        = tree
+  where
+    right (Node (Node llt lv rlt) v rt) = Node llt lv (Node rlt v rt)
+    right t = t
+    left (Node lt v (Node lrt rv rrt)) = Node (Node lt v lrt) rv rrt
+    left t = t
+    leftright (Node (Node Leaf lv (Node lrlt rlv rrlt)) v Leaf) =
+      (Node (Node Leaf lv lrlt) rlv (Node rrlt v Leaf))
+    leftright t = t
+    rightleft (Node Leaf v (Node (Node llrt lrv rlrt) rv Leaf)) =
+      (Node (Node Leaf v rlrt) lrv (Node llrt rv Leaf))
+    rightleft t = t
+
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) tree = tree
+insert msg Leaf = Node Leaf msg Leaf
+insert msg (Node lt (Unknown _) rt) = (Node lt msg rt)
+insert msg@(LogMessage _ timestamp _) (Node lt omsg@(LogMessage _ v _) rt)
+  | v > timestamp = rotate (Node (insert msg lt) omsg rt)
+  | v < timestamp = rotate (Node lt omsg (insert msg rt))
+  | otherwise     = rotate (Node lt msg rt)
+
+build :: [LogMessage] -> MessageTree
+build l = foldr insert Leaf l
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf           = []
+inOrder (Node lt v rt) = (inOrder lt) ++ [v] ++ (inOrder rt)
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong [] = []
+whatWentWrong (r: rs) =
+  case r of
+    Unknown _              -> whatWentWrong rs
+    LogMessage Info _ _    -> whatWentWrong rs
+    LogMessage Warning _ _ -> whatWentWrong rs
+    LogMessage (Error i) _ s -> (if (i > 50) then [s] else []) ++ (whatWentWrong rs)
 
 main :: IO ()
 main = do
