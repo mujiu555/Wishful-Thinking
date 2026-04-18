eb743e595f6de0ad500d5b7efeb0e0deeb269fe4
mujiu555@GitHub <1310769510@qq.com>
2026-04-19 01:00:32 +0800

    feat(CIS194): exercise IV

--- src/CIS194/src/E4.hs
+++ src/CIS194/src/E4.hs
@@ -1,68 +1,74 @@
--- |
-
 module E4 where
+
+import Data.List
 
 -- Exercise I
 
 fun1o :: [Integer] -> Integer
-fun1o []      = 1
-fun1o (x:xs)
-  | even x    = (x-2) * fun1o xs
-  | otherwise = fun1o xs
+fun1o [] = 1
+fun1o (x : xs)
+    | even x = (x - 2) * fun1o xs
+    | otherwise = fun1o xs
 
 fun1 :: [Integer] -> Integer
 fun1 = product . map (flip (-) 2) . filter even
 
 fun2o :: Integer -> Integer
 fun2o 1 = 0
-fun2o n | even n    = n + fun2o (n `div` 2)
-        | otherwise = fun2o (3 * n + 1)
+fun2o n
+    | even n = n + fun2o (n `div` 2)
+    | otherwise = fun2o (3 * n + 1)
 
 fun2 :: Integer -> Integer
-fun2 = sum . filter even . takeWhile (/= 1) . iterate
-  (\x -> if even x then x `div` 2 else 1 + 3 * x)
+fun2 =
+    sum
+        . filter even
+        . takeWhile (/= 1)
+        . iterate
+            (\x -> if even x then x `div` 2 else 1 + 3 * x)
 
 -- Exercise II
 
-data Tree a =
-  Leaf
-  | Node Integer (Tree a) a (Tree a)
-  deriving (Show, Eq)
+data Tree a
+    = Leaf
+    | Node Integer (Tree a) a (Tree a)
+    deriving (Show, Eq)
 
 height :: Tree a -> Integer
-height Leaf           = 0
+height Leaf = 0
 height (Node _ l _ r) = 1 + max (height l) (height r)
 
 balance :: Tree a -> Integer
-balance Leaf           = 0
-balance (Node h l _ r) = height l - height r
+balance Leaf = 0
+balance (Node _ l _ r) = height l - height r
 
 balanced :: Tree a -> Bool
 balanced = (<= 0) . balance
 
-rotate :: Tree -> Tree
-rotate Leaf = Leaf
-rotate tree@(Node h ltree _ rtree)
-  | balance tree > 1 && balance ltree > 0  = right tree
-  | balance tree < -1 && balance rtree < 0 = left tree
-  | balance tree > 1 && balance ltree < 0  = leftright tree
-  | balance tree < -1 && balance rtree > 0 = rightleft tree
-  | otherwise                        = tree
-  where
-    right (Node (Node llt lv rlt) v rt) = Node llt lv (Node rlt v rt)
-    right t = t
-    left (Node lt v (Node lrt rv rrt)) = Node (Node lt v lrt) rv rrt
-    left t = t
-    leftright (Node (Node Leaf lv (Node lrlt rlv rrlt)) v Leaf) =
-      Node (Node Leaf lv lrlt) rlv (Node rrlt v Leaf)
-    leftright t = t
-    rightleft (Node Leaf v (Node (Node llrt lrv rlrt) rv Leaf)) =
-      Node (Node Leaf v rlrt) lrv (Node llrt rv Leaf)
-    rightleft t = t
-
-insert :: Tree a -> val -> Tree a
-insert Leaf a           = Node 1 Leaf a Leaf
-insert (Node h lt v rt) = . -- TODO
+insertT :: a -> Tree a -> Tree a
+insertT val Leaf = Node 1 Leaf val Leaf
+insertT val (Node _ lt ov rt) =
+  Node (1 + max (height lt) (height rt)) Leaf val Leaf
 
 foldTree :: [a] -> Tree a
-foldTree = foldr insert Leaf
+foldTree = foldr insertT Leaf
+
+-- Exercise III
+
+xor :: [Bool] -> Bool
+-- xor l = odd (length (foldr (\x y -> if x then x: y else y) [] l))
+xor = foldr (\x y -> not (x && y) && (x || y)) False
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x y -> f x : y) []
+
+foldl'' :: (a -> b -> a) -> a -> [b] -> a
+foldl'' f = foldr (flip f)
+
+-- Exercise IV
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x, y) | x <- xs, y <- ys]
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = map ((+ 1) . (* 2)) ([1 .. n] \\ [i + j + 2 * i * j | j <- [1 .. n], i <- [1 .. j], i + j + 2 * i * j <= n])
