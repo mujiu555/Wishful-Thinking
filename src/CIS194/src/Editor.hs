2be4f3d4ec3ee7198a35d9c8bba1d88f1cd5fd56
mujiu555@GitHub <1310769510@qq.com>
2026-05-17 03:58:07 +0800

    feat(CIS194): Coursework VII, fix some minor erros of coursework

--- src/CIS194/src/Editor.hs
+++ src/CIS194/src/Editor.hs
@@ -12,6 +12,7 @@ import Control.Monad.State
 
 import Control.Applicative
 import Control.Arrow       (first, second)
+import Control.Monad
 
 import Data.Char
 import Data.List
@@ -35,7 +36,7 @@ commands = map show [View, Edit, Next, Prev, Quit]
 -- Editor monad
 
 newtype Editor b a = Editor (StateT (b,Int) IO a)
-  deriving (Functor, Monad, MonadIO, MonadState (b,Int))
+  deriving (Functor, Applicative, Monad, MonadIO, MonadState (b,Int))
 
 runEditor :: Buffer b => Editor b a -> b -> IO a
 runEditor (Editor e) b = evalStateT e (b,0)
