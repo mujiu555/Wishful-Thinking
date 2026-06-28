03320417e93847a1d56fe51e5c6ab877c12adbc6
Mujiu555 <133623533+mujiu555@users.noreply.github.com>
2026-06-28 23:51:40 +0800

    feat(CIS194): Course Work X, A Parser

--- src/CIS194/src/AParser.hs
+++ src/CIS194/src/AParser.hs
@@ -3,6 +3,8 @@
 module AParser where
 
 import Data.Char
+import Control.Applicative
+import Control.Monad
 
 newtype Parser a =
   Parser { runParser :: String -> Maybe (a, String) }
@@ -33,4 +35,52 @@ first :: (a -> b) -> (a,c) -> (b,c)
 first f (a,b) = (f a, b)
 
 instance Functor Parser where
+  -- fmap :: (a -> b) -> f a -> f b
   fmap f (Parser p) = Parser $ fmap (first f) . p
+  -- p :: String -> Maybe (a, Str)
+  -- f :: a -> b
+  -- first f :: (a, c) -> (b, c)
+  -- fmap :: ((a, b) -> (b, c)) -> f (a, b) -> f (b, c)
+  -- fmap (first f) :: f (a, b) -> f (b, c)
+  -- \str -> fmap (first f) (p str)
+
+-- Exercise II
+instance Applicative Parser where
+  -- pure :: Applicative f => a -> f a
+  pure a = Parser (\x -> Just (a, x))
+  -- pure :: a -> Parser a
+
+  -- <*> :: f (a -> b) -> f a -> f b
+  (<*>) (Parser p1) (Parser f2) = Parser $ \src -> do
+    (h, src') <- p1 src
+    (x, src'') <- f2 src'
+    pure (h x, src'')
+
+-- Exercise III
+abParser :: Parser (Char, Char)
+abParser = (,) <$> char 'a' <*> char 'b'
+-- (,) :: a -> b -> (a, b)
+-- (,) :: a -> (b -> (a, b))
+-- char :: Char -> Parser Char
+-- char c :: Parser Char
+-- (<$>) :: (a -> b) -> f a -> f b
+-- (<$>) :: (a -> b) -> Parser a -> Parser b
+-- (<*>) :: f (a -> b) -> f a -> f b
+-- (,) <$> :: Parser a -> Parser (b -> (a, b))
+-- (,) <$> char 'a' :: Parser (b -> (a, b))
+-- (,) <$> char 'a' <*> :: Parser a -> Parser (a, b)
+
+abParser_ :: Parser ()
+abParser_ = (\_ _ -> ()) <$> char 'a' <*> char 'b'
+
+intPair :: Parser [Integer]
+intPair = (\x _ z -> [x, z]) <$> posInt <*> char ' ' <*> posInt
+
+-- Exercise IV
+instance Alternative Parser where
+  empty = Parser (const Nothing)
+  Parser a <|> Parser b = Parser (\str -> a str <|> b str)
+
+-- Exercise V
+intOrUppercase :: Parser ()
+intOrUppercase =  void posInt <|> void (satisfy isUpper)
