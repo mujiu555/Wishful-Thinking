18b543bf17d7afe190f13aa5ca8cdee2cbe72674
Mujiu555 <133623533+mujiu555@users.noreply.github.com>
2026-06-29 03:33:18 +0800

    feat(CIS194): WIP! Course work XI, exercise I

--- src/CIS194/src/AParser.hs
+++ src/CIS194/src/AParser.hs
@@ -1,23 +1,17 @@
--- |
+module AParser (Parser, runParser, satisfy, char, posInt) where
 
-module AParser where
+import           Control.Applicative
+import           Data.Char
 
-import Data.Char
-import Control.Applicative
-import Control.Monad
-
-newtype Parser a =
-  Parser { runParser :: String -> Maybe (a, String) }
+newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
 
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = Parser f
   where
-    f [] = Nothing -- fail on the empty input
-    f (x:xs) -- check if x satisfies the predicate
-      -- if so, return x along with the remainder
-      -- of the input (that is, xs)
-      | p x = Just (x, xs)
-      | otherwise = Nothing -- otherwise, fail
+    f [] = Nothing
+    f (x:xs)
+        | p x       = Just (x, xs)
+        | otherwise = Nothing
 
 char :: Char -> Parser Char
 char c = satisfy (== c)
@@ -26,61 +20,25 @@ posInt :: Parser Integer
 posInt = Parser f
   where
     f xs
-      | null ns = Nothing
+      | null ns   = Nothing
       | otherwise = Just (read ns, rest)
       where (ns, rest) = span isDigit xs
 
--- Exercise I
+inParser f = Parser . f . runParser
+
 first :: (a -> b) -> (a,c) -> (b,c)
-first f (a,b) = (f a, b)
+first f (x,y) = (f x, y)
 
 instance Functor Parser where
-  -- fmap :: (a -> b) -> f a -> f b
-  fmap f (Parser p) = Parser $ fmap (first f) . p
-  -- p :: String -> Maybe (a, Str)
-  -- f :: a -> b
-  -- first f :: (a, c) -> (b, c)
-  -- fmap :: ((a, b) -> (b, c)) -> f (a, b) -> f (b, c)
-  -- fmap (first f) :: f (a, b) -> f (b, c)
-  -- \str -> fmap (first f) (p str)
+  fmap = inParser . fmap . fmap . first
 
--- Exercise II
 instance Applicative Parser where
-  -- pure :: Applicative f => a -> f a
-  pure a = Parser (\x -> Just (a, x))
-  -- pure :: a -> Parser a
+  pure a = Parser (\s -> Just (a, s))
+  (Parser fp) <*> xp = Parser $ \s ->
+    case fp s of
+      Nothing     -> Nothing
+      Just (f,s') -> runParser (f <$> xp) s'
 
-  -- <*> :: f (a -> b) -> f a -> f b
-  (<*>) (Parser p1) (Parser f2) = Parser $ \src -> do
-    (h, src') <- p1 src
-    (x, src'') <- f2 src'
-    pure (h x, src'')
-
--- Exercise III
-abParser :: Parser (Char, Char)
-abParser = (,) <$> char 'a' <*> char 'b'
--- (,) :: a -> b -> (a, b)
--- (,) :: a -> (b -> (a, b))
--- char :: Char -> Parser Char
--- char c :: Parser Char
--- (<$>) :: (a -> b) -> f a -> f b
--- (<$>) :: (a -> b) -> Parser a -> Parser b
--- (<*>) :: f (a -> b) -> f a -> f b
--- (,) <$> :: Parser a -> Parser (b -> (a, b))
--- (,) <$> char 'a' :: Parser (b -> (a, b))
--- (,) <$> char 'a' <*> :: Parser a -> Parser (a, b)
-
-abParser_ :: Parser ()
-abParser_ = (\_ _ -> ()) <$> char 'a' <*> char 'b'
-
-intPair :: Parser [Integer]
-intPair = (\x _ z -> [x, z]) <$> posInt <*> char ' ' <*> posInt
-
--- Exercise IV
 instance Alternative Parser where
   empty = Parser (const Nothing)
-  Parser a <|> Parser b = Parser (\str -> a str <|> b str)
-
--- Exercise V
-intOrUppercase :: Parser ()
-intOrUppercase =  void posInt <|> void (satisfy isUpper)
+  Parser p1 <|> Parser p2 = Parser $ liftA2 (<|>) p1 p2
